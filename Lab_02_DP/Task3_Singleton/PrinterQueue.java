2a05931a2adba14beea4300593ef2b65222b8624
Ke Ning <u7175553@anu.edu.au>
2021-08-16 18:04:45 +0800

    task3 done!

--- Lab_02_DP/Task3_Singleton/PrinterQueue.java
+++ Lab_02_DP/Task3_Singleton/PrinterQueue.java
@@ -2,15 +2,19 @@ package Task3_Singleton;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+//1. static 的用法。class中含有static自身的static instance    、、  以上情况中仍然能够出现instance field，如本task的instance.queue
+//2. static 的 access
 
 /**
  * A queue (first-in, first-out) which processes printing requests for a printer.
  * Note that there is only ever 1 instance of the queue. Therefore it implements
- *  the singleton design pattern.
- *
+ * the singleton design pattern.
+ * <p>
  * Just like the other tasks, you may edit this class as
- *  much as you like (create helper methods if you want!) So long as you
- *  genuinely pass the tests.
+ * much as you like (create helper methods if you want!) So long as you
+ * genuinely pass the tests.
  */
 public class PrinterQueue implements SimpleQueue<String> {
     /*
@@ -22,17 +26,20 @@ public class PrinterQueue implements SimpleQueue<String> {
 
     /**
      * Currently we use a list for our queue. Perhaps... there is a better data structure... ;)
-     *  Note that you can complete the whole task with a list. However, you can make life easier
-     *  by choosing the right data structure as we will find out later in this course.
+     * Note that you can complete the whole task with a list. However, you can make life easier
+     * by choosing the right data structure as we will find out later in this course.
      */
     private List<String> queue = new ArrayList<>();
+    private static PrinterQueue instance = null;
 
     // TODO: store an instance of the printer queue.
 
     /**
      * Private constructor so it can only be accessed from within the class
      */
-    private PrinterQueue() {};
+    private PrinterQueue() {
+
+    }
 
     /**
      * @return new instance if it currently does not exist and current instance if it already exists.
@@ -42,36 +49,61 @@ public class PrinterQueue implements SimpleQueue<String> {
          TODO: implement this method
          Note that you will need to create an instance of PrinterQueue.
          */
-        return null;
+
+        if (instance == null) {
+            instance = new PrinterQueue();
+        }
+
+        return instance;
     }
 
     /**
      * Adds element to the end of the print queue
+     *
      * @return true if successful, false otherwise.
      */
     @Override
     public boolean add(String paper) {
         // TODO: implement this method
+        if (paper == null) return false;
+        if (!instance.queue.contains(paper)) {
+            instance.queue.add(paper);
+            return true;
+        }
+
         return false;
+
     }
 
     /**
      * Returns the element at the front of the queue
+     *
      * @return string at the front of queue or returns null if this queue is empty.
      */
     @Override
     public String peek() {
         // TODO: implement this method
-        return null;
+        if (instance.queue.size()==0){
+            return null;
+        }
+
+        return instance.queue.get(0);
     }
 
     /**
      * Removes and returns the element at the front of the queue
+     *
      * @return String at the front of the queue or returns null if this queue is empty.
      */
     @Override
     public String poll() {
         // TODO: implement this method
-        return null;
+        if (instance.queue.size()==0){
+            return null;
+        }
+        String old= instance.queue.get(0);
+        List<String> demo= instance.queue.subList(1,instance.queue.size());
+        instance.queue=demo;
+        return old;
     }
 }
